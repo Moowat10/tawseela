33f6d812f2d6fb076b80b758f1527ba34a15af6f
Ahmed3mr <60897778+Ahmed3mr@users.noreply.github.com>
2021-12-22 03:48:05 +0200

    translation

--- src/assets/lang/en.js
+++ src/assets/lang/en.js
@@ -1,10 +1,41 @@
 export default{
+    homeview: {
+
+        m6:"Ride your",
+        m7:"bike",
+        m8:"Register Now",
+        m9:"R E G I S T E R A T I O N",
+        m10:"Name",
+        m11:"National ID",
+        m12:"Phone Number",
+        m13:"Address",
+        m14:"Email",
+        m15:"Password",
+        m16:"Confirm Password",
+        m17:"Or",
+        m18:"do",
+        m19:"you",
+        m20:"prefer",
+        m21:"a quick",
+        m22:"start?",
+
+    },
     header: {
-        title: "M A N D A R I N",
-        m1: "INFORMATION",
-        m2: "SUPPORT",
-        m3: "PRODUCT",
-        m4: "CONTACT",
-        searchtxt: "Type to search.."
+
+        h1:"Login",        
+
+    },
+    profileview: {
+        p1:"Email : ",
+        p2:"National ID : ",
+        p3:"Address : ",
+        p4: "Phone Number : ",
+        p5:"Kilometers",
+        p6:"2061",
+        p7:"km",
+        p8:"Rides",
+        p9:"20",
+        p10:"Calories",
+        p11:"calorie"
     }
 }
